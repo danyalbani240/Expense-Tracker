4057dfac45cb8dee0c7f1bdc03418908b27a3011
Danyal Bani <mr.d.bani.d@gmail.com>
2022-07-01 19:07:31 +0330

    some new style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import ExpenseTracker from "./components/ExpenseTracker";
 
 function App() {
 	return (
 		<div className="App">
 			<h2>Expense Tracker</h2>
+			<ExpenseTracker />
 		</div>
 	);
 }
