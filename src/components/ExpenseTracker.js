570506dcaa673197419810e321c0c7f082e9ad0c
Danyal <mr.d.bani.d@gmail.com>
2022-07-09 12:38:29 +0330

    adding transactions to localStorage for saving data

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -3,11 +3,46 @@ import Overview from "./Overview";
 import Transactions from "./Transactions";
 
 const ExpenseTracker = () => {
-	const [expense, setExpense] = useState(0);
-	const [income, setIncome] = useState(0);
-	const [transactions, setTransactions] = useState([]);
+	const calculateExpense = () => {
+		const transactions = JSON.parse(localStorage.getItem("transactions"));
+		let expenses = 0;
+		for (const transaction of transactions) {
+			if (transaction.type === "expense") {
+				expenses += +transaction.amount;
+			}
+		}
+		return expenses;
+	};
+	const calculateIncome = () => {
+		const transactions = JSON.parse(localStorage.getItem("transactions"));
+		let incomes = 0;
+		for (const transaction of transactions) {
+			if (transaction.type === "income") {
+				incomes += +transaction.amount;
+			}
+		}
+		return incomes;
+	};
+	const [expense, setExpense] = useState(
+		localStorage.transactions === undefined ? 0 : calculateExpense()
+	);
+	const [income, setIncome] = useState(
+		localStorage.transactions === undefined ? 0 : calculateIncome()
+	);
+	const [transactions, setTransactions] = useState(
+		localStorage.transactions === undefined
+			? []
+			: JSON.parse(localStorage.getItem("transactions"))
+	);
+
 	const addTransaction = (data) => {
-		setTransactions([...transactions, { ...data, id: Date.now() }]);
+		const newTransaction = { ...data, id: Date.now() };
+		setTransactions([...transactions, newTransaction]);
+
+		localStorage.setItem(
+			"transactions",
+			JSON.stringify([...transactions, newTransaction])
+		);
 		if (data.type === "income") {
 			setIncome((oldValue) => oldValue + +data.amount);
 		} else {
